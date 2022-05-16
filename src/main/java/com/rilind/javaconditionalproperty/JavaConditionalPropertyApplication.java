b21c69c9ad6fea88c97c7b190f6a36d371edd07e
RIlind Nuka <rilindnuka@gmail.com>
2022-05-16 11:57:24 +0200

    initial  application commit

---
+++ src/main/java/com/rilind/javaconditionalproperty/JavaConditionalPropertyApplication.java
@@ -0,0 +1,13 @@
+package com.rilind.javaconditionalproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConditionalPropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaConditionalPropertyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rilind/javaconditionalproperty/JavaConditionalPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rilind.javaconditionalproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConditionalPropertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
